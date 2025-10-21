bf991742b28de62567fd44b7a3c33ebccf5207f5
asli.sahin <asli.sahin@kamutech.com.tr>
2025-10-21 13:20:57 +0300

    switch structures removed from factory

---
+++ DesignPatternsDemo/Discount/DiscountStrategyFactory.cs
@@ -0,0 +1,13 @@
+﻿using DesignPatternsDemo.Domain;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DesignPatternsDemo.Discount;
+
+public sealed class DiscountStrategyFactory : IDiscountStrategyFactory
+{
+    private readonly IServiceProvider _sp;
+    
+    public DiscountStrategyFactory (IServiceProvider sp) => _sp = sp;
+    
+    public IDiscountStrategy Create(CustomerType type)
+        => _sp.GetRequiredKeyedService<IDiscountStrategy>(type);}
--- DesignPatternsDemo/Shipping/ExpressShipping.cs
+++ DesignPatternsDemo/Shipping/ExpressShipping.cs
@@ -10,12 +10,6 @@ namespace DesignPatternsDemo.Shipping
     public sealed class ExpressShipping : IShippingService
     {
         public string Name => "Express Shipping";
-        public decimal CalculateShippingCost(Order order)
-        {
-            //ekspres kargo hesaplama örneği
-            decimal baseRate = 15.00m; // Sabit başlangıç ücreti
-            decimal weightRate = 1.00m; // Ağırlık başına ücret
-            return baseRate + (weightRate * order.Weight);
-        }
+        public decimal CalculateShippingCost(Order order) => 15.00m + (1.00m * order.Weight);
     }
 }
